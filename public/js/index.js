1218deb322bd0c667c7e9bffca9818f7bc6214c3
Ben Botvinick <benbotvinick@gmail.com>
2019-08-04 02:27:39 -0400

    reconfig

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from '../../components/App';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
+import ScrollToTop from 'react-router-scroll-top';
+import Home from '../../pages';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 // Initialize Font Awesome
 library.add(fas);
 
 // Render the app
-render(<App />, document.getElementById('root'));
+render((
+	<BrowserRouter>
+		<ScrollToTop>
+			<Switch>
+				<Route exact path='/' component={Home} />
+			</Switch>
+		</ScrollToTop>
+	</BrowserRouter>
+), document.getElementById('root'));
